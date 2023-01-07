855dcbc05f411f813b1c2a200ddc97e3bed13ca9
Domenic Wolf <53898442+Splashpixx@users.noreply.github.com>
2023-01-07 15:46:01 +0100

    Update App.js
    Clickable qube - if clicked -> Color change, position change, size change

--- React-three-js/src/App.js
+++ React-three-js/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 // https://www.youtube.com/watch?v=kxXaIHi1j4w
 import { Canvas, extend } from '@react-three/fiber';
 import glsl from "babel-plugin-glsl/macro"
-import { shaderMaterial } from '@react-three/drei';
+import { PositionPoint, shaderMaterial } from '@react-three/drei';
 import * as THREE from "three";
 
 // https://docs.pmnd.rs/react-three-fiber/tutorials/loading-models
@@ -10,7 +10,7 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { useLoader } from '@react-three/fiber'
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { Suspense } from "react";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 
 //https://codesandbox.io/s/basic-clerping-example-qh8vhf?file=/src/Scene.js
 import { PerspectiveCamera } from "@react-three/drei";
@@ -18,6 +18,8 @@ import { Box, Plane, Text } from "@react-three/drei";
 import { useEffect, useState, useRef  } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 
+//https://onion2k.github.io/r3f-by-example/examples/hooks/rotating-cube/
+import { render } from "react-dom";
 
 const Scene = () => {
   const cube = useRef()
@@ -31,52 +33,64 @@ const Scene = () => {
     loader.setMaterials(materials);
   });
 
+
+  const Box = () => {
+    const boxRef = useRef();
+    const [active, setActive] = useState(false)
+  
+    useFrame(() => {
+      boxRef.current.rotation.y += 0.01;
+    });
+  
+    return (
+      <mesh position={active ? [36.5,13,39.5]  : [36.5,12,39.5]} ref={boxRef} rotation-x={Math.PI * 0.25} rotation-y={Math.PI * 0.25}
+      
+      scale={active ? 1.5 : 1} onClick={() => setActive(!active)} 
+        
+
+      >
+        <boxBufferGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial color={active ? "green" : "red"} />
+      </mesh>
+    );
+  };
+
+  
+
  
   return ( 
   <Canvas>
       
-      <OrbitControls makeDefault position={[20, 10, 40]} />
+      <OrbitControls/>
       
       <ambientLight 
       intensity={0.5}
-      color={x}
       />
+
       <directionalLight
         castShadow
         shadow-mapSize-height={512}
         shadow-mapSize-width={512}
         intensity={0.8}
       />
-
-      <Text
-        color="black" // default
-        anchorX="center" // default
-        anchorY="middle" // default
-        rotation={[0, 0, 0]}
-        position={[0, 15, -5]}
-        fontSize={2}
-      >
-        Hello World
-      </Text>
       
       <Suspense fallback={null}>
           <primitive 
-            object={obj} 
+            object={obj}
+            position={[0, 0, 0]}
             onClick={(e) => {
               if(x == "green"){
                 x = "blue"
               } else {
                 x = "green"
               }
-
-              console.log(x)
-            
+              console.log(obj)
             }}
             onWheel={(e) => console.log('wheel spins')}
           />
-          
       </Suspense>  
-      
+
+      <Box />
   </Canvas>
   );
 };
