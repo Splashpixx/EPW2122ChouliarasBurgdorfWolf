b4b1107168960fbaa934c247c45f6d087e770d5e
BurgJens <91873641+BurgJens@users.noreply.github.com>
2023-01-08 00:50:22 +0100

    implement and test A_Star_Variant

--- Artefakte/RapidPrototypes/ThreeJS_RapidPrototype/ThreeJS_RapidPrototype_A_Star_Variant/code/main.js
+++ Artefakte/RapidPrototypes/ThreeJS_RapidPrototype/ThreeJS_RapidPrototype_A_Star_Variant/code/main.js
@@ -1,44 +1,13 @@
 async function main()
 {
-    const derPath = "obj/pathMesh.obj";
-    // const derPath = "obj/pathMesh_TEST.obj";
-    let pathMesh = await importPathMesh(derPath);
+    const pathMeshPath = "obj/pathMesh.obj";
+    // const pathMeshPath = "obj/pathMesh_TEST.obj";
+    const pathMesh = await importPathMesh(pathMeshPath);
     console.log(pathMesh)
 
-    const actualPathPoints = [];
+    let path = await findPath(pathMesh[34], pathMesh[164], pathMesh)
 
-    //befüllen des Arrays mit Beispielpunkten, ohne edges, da referenzierung noch nicht möglich
-    actualPathPoints.push(new PathMeshPoint(0, new THREE.Vector3(2, 0, 11)))
-    actualPathPoints.push(new PathMeshPoint(1, new THREE.Vector3(3, 0, 11)))
-    actualPathPoints.push(new PathMeshPoint(2, new THREE.Vector3(3, 5, 11)))
-    actualPathPoints.push(new PathMeshPoint(3, new THREE.Vector3(4, 5, 11)))
-    actualPathPoints.push(new PathMeshPoint(4, new THREE.Vector3(3, 7, 11)))
-    actualPathPoints.push(new PathMeshPoint(5, new THREE.Vector3(3, 7, 1)))
-    actualPathPoints.push(new PathMeshPoint(6, new THREE.Vector3(3, 3, 1)))
-    actualPathPoints.push(new PathMeshPoint(7, new THREE.Vector3(3, 11, 1)))
-    actualPathPoints.push(new PathMeshPoint(8, new THREE.Vector3(4, 11, 1)))
-
-    actualPathPoints[0].edges.push(new Edge(actualPathPoints[1],0))
-    actualPathPoints[1].edges.push(new Edge(actualPathPoints[0],0))
-    actualPathPoints[1].edges.push(new Edge(actualPathPoints[2],0))
-    actualPathPoints[2].edges.push(new Edge(actualPathPoints[1],0))
-    actualPathPoints[2].edges.push(new Edge(actualPathPoints[3],0))
-    actualPathPoints[2].edges.push(new Edge(actualPathPoints[4],0))
-    actualPathPoints[3].edges.push(new Edge(actualPathPoints[2],0))
-    actualPathPoints[4].edges.push(new Edge(actualPathPoints[2],0))
-    actualPathPoints[4].edges.push(new Edge(actualPathPoints[5],0))
-    actualPathPoints[5].edges.push(new Edge(actualPathPoints[6],0))
-    actualPathPoints[5].edges.push(new Edge(actualPathPoints[7],0))
-    actualPathPoints[6].edges.push(new Edge(actualPathPoints[5],0))
-    actualPathPoints[7].edges.push(new Edge(actualPathPoints[5],0))
-    actualPathPoints[7].edges.push(new Edge(actualPathPoints[8],0))
-    actualPathPoints[8].edges.push(new Edge(actualPathPoints[7],0))
-
-    console.log(actualPathPoints);
-
-// Beispiel Start und Endunte werden aus der Liste ausgewähl
-
-    var path = findPath(actualPathPoints[8], actualPathPoints[0], actualPathPoints)
+    console.log(path)
 }
 
 main();
